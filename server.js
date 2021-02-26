6f31ab3f79c2adf17ab48effdfca4e744a892aae
jbeard <player69>
2021-02-25 18:09:38 -0700

    add methods to capture user input and write to animals.json file

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const { animals } = require('./data/animals.json');
+const fs = require('fs');
+const path = require('path');
 
 const express = require('express');
+const { type } = require('os');
+const e = require('express');
 const app = express();
-const PORT = process.env.PORT || 3001;
+// pare incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming json data
+app.use(express.json());
+const PORT = process.env.PORT || 3002;
 
 
 function filterByQuery(query, animalsArray) {
@@ -48,6 +56,44 @@ function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
 }
+
+function validateAnimal(animal){
+    if(!animal.name || typeof animal.name !== 'string'){
+        return false;
+    }
+    if(!animal.species || typeof animal.species !== 'string'){
+        return false;
+    }
+    if(!animal.diet || typeof animal.diet !== 'string'){
+        return false;
+    }
+    if(!animal.personalityTraits || !Array.isArray(animal.personalityTraits)){
+        return false;
+    }
+    return true;
+}
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(path.join(__dirname, './data/animals.json'), 
+    JSON.stringify({animals : animalsArray}, null, 2)
+    );
+    return animal;
+}
+
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+    
+    // add animal to json file and animals array in this function
+    if(!validateAnimal(req.body)){
+        res.status(400).send('The animal is not properly formatted.');
+    } else{
+        const animal = createNewAnimal(req.body, animals);
+        res.json(req.body);
+    }
+})
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
